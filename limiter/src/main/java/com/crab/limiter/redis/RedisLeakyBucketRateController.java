4ebb742b8adbfd6334098b795429bee388bf83b1
dongchao <hdbysj@163.com>
2022-09-19 16:52:33 +0800

    init:limiter package adjust

---
+++ limiter/src/main/java/com/crab/limiter/redis/RedisLeakyBucketRateController.java
@@ -0,0 +1,10 @@
+package com.crab.limiter.redis;
+
+/**
+ * Redis leaky bucket rate
+ *
+ * @author dongchao
+ * @Date 2022/9/19 4:43 PM
+ */
+public class RedisLeakyBucketRateController {
+}
---
+++ limiter/src/main/java/com/crab/limiter/redis/RedisSlidingWindowRateController.java
@@ -0,0 +1,10 @@
+package com.crab.limiter.redis;
+
+/**
+ * redis sling window rate impl
+ *
+ * @author dongchao
+ * @Date 2022/9/19 4:36 PM
+ */
+public class RedisSlidingWindowRateController {
+}
