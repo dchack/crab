949f24ca4ef32d199342f8ecbd556098944d9fb3
dongchao <hdbysj@163.com>
2022-09-22 22:42:10 +0800

    init:add sliding window rate limit lua

--- limiter/src/main/java/com/crab/limiter/redis/RedisSlidingWindowRateController.java
+++ limiter/src/main/java/com/crab/limiter/redis/RedisSlidingWindowRateController.java
@@ -1,10 +1,28 @@
 package com.crab.limiter.redis;
 
+import com.crab.limiter.RateController;
+import com.crab.limiter.rate.RateInfo;
+import org.springframework.data.redis.core.RedisTemplate;
+
 /**
- * redis sling window rate impl
+ * Sling window rate impl by Redis
  *
  * @author dongchao
  * @Date 2022/9/19 4:36 PM
  */
-public class RedisSlidingWindowRateController {
+public class RedisSlidingWindowRateController implements RateController {
+
+    private RedisTemplate<String, String> redisTemplate;
+
+    public RedisSlidingWindowRateController(RedisTemplate<String, String> redisTemplate) {
+        this.redisTemplate = redisTemplate;
+    }
+
+    @Override
+    public boolean isAllow(RateInfo rateInfo) {
+
+
+
+        return false;
+    }
 }
