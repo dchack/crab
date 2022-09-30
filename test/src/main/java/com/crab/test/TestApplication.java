e606f41e720e1e55baa72c8f65994f6877eda203
dongchao <hdbysj@163.com>
2022-09-30 17:28:23 +0800

    Daily commit:idempotent support mysql

--- test/src/main/java/com/crab/test/TestApplication.java
+++ test/src/main/java/com/crab/test/TestApplication.java
@@ -1,9 +1,7 @@
 package com.crab.test;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
